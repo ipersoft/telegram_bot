96b47a09c76757734378a53c00e392a20b073639
Maurizio De Magnis <root@olisti.co>
2016-09-15 14:48:40 +0200

    applies a coherent indentation
    uses 2 spaces

--- DESIOPPLUGIN/now.php
+++ DESIOPPLUGIN/now.php
@@ -1,20 +1,18 @@
 <?php
-$meteo = file_get_contents('http://api.wunderground.com/api/TOKEN/conditions/lang:IT/q/Italy/Desio.json');
-$json = json_decode($meteo, true);
+  $meteo = file_get_contents('http://api.wunderground.com/api/TOKEN/conditions/lang:IT/q/Italy/Desio.json');
+  $json = json_decode($meteo, true);
 
-    
-    $condizione = $json['current_observation']['weather'];
-    $temp= $json['current_observation']['temp_c'];
-    $umidita=$json['current_observation']['relative_humidity'];
-    $icon=$json['current_observation']['icon'];
-switch ($icon) {
+  $condizione = $json['current_observation']['weather'];
+  $temp= $json['current_observation']['temp_c'];
+  $umidita=$json['current_observation']['relative_humidity'];
+  $icon=$json['current_observation']['icon'];
+
+  switch ($icon) {
     case "rain":
-        $icont=":umbrella:";
-        break;
-}
+      $icont=":umbrella:";
+      break;
+  }
 
-    echo 'Condizioni: '.$condizione." ".$icont."\r\n";
-    echo 'Temperatura: '.$temp."° C\r\n";
-    echo 'Umidità: '.$umidita."\r\n";
-
-   
+  echo 'Condizioni: '.$condizione." ".$icont."\r\n";
+  echo 'Temperatura: '.$temp."° C\r\n";
+  echo 'Umidità: '.$umidita."\r\n";
